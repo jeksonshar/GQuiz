5c388d9643c8d06bf387f7d3abec15d24fbb15af
Javernaut <javernaut@gmail.com>
2020-03-14 12:13:03 +0200

    Simple quiz

--- app/src/main/java/com/javernaut/gquiz/MainActivity.java
+++ app/src/main/java/com/javernaut/gquiz/MainActivity.java
@@ -3,12 +3,39 @@ package com.javernaut.gquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button trueButton;
+    private Button falseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        trueButton = findViewById(R.id.true_button);
+        falseButton = findViewById(R.id.false_button);
+
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast(R.string.correct_toast);
+            }
+        });
+
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast(R.string.incorrect_toast);
+            }
+        });
+    }
+
+    private void showToast(int textId) {
+        Toast.makeText(MainActivity.this, textId, Toast.LENGTH_SHORT).show();
     }
 }
