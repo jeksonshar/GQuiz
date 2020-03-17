76dedcc7c7b9e949fc3f96a8c97a8f941ed869b4
Javernaut <javernaut@gmail.com>
2020-03-17 21:40:25 +0200

    Adding logging for MainActivity

--- app/src/main/java/com/javernaut/gquiz/MainActivity.java
+++ app/src/main/java/com/javernaut/gquiz/MainActivity.java
@@ -1,14 +1,12 @@
 package com.javernaut.gquiz;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends LoggingActivity {
 
     private Button trueButton;
     private Button falseButton;
