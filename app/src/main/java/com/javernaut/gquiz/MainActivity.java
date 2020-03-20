2c4e9bf6446c31faf4b5a6e4175627c88f148958
Jekson <jeksonshar@gmail.com>
2020-03-20 16:32:09 +0200

    corrected comments of Alexander

--- app/src/main/java/com/javernaut/gquiz/MainActivity.java
+++ app/src/main/java/com/javernaut/gquiz/MainActivity.java
@@ -1,7 +1,6 @@
 package com.javernaut.gquiz;
 
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,7 +14,7 @@ public class MainActivity extends LoggingActivity {
     private Button previousButton;
     private TextView questionView;
 
-    private int currantQuestionIndex = 0;
+    private int currentQuestionIndex = 0;
 
     // база данных вопросов приложения
     private Question[] mQuestionBank = new Question[] {
@@ -28,16 +27,19 @@ public class MainActivity extends LoggingActivity {
     };
 
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         questionView = findViewById(R.id.question);
+        trueButton = findViewById(R.id.true_button);
+        falseButton = findViewById(R.id.false_button);
+        nextButton = findViewById(R.id.next_button);
+        previousButton = findViewById(R.id.previous_button);
 
         applyCurrentQuestion();
 
         // обработка кнопки True
-        trueButton = findViewById(R.id.true_button);
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,7 +48,6 @@ public class MainActivity extends LoggingActivity {
         });
 
         // обработка кнопки False
-        falseButton = findViewById(R.id.false_button);
         falseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,15 +56,14 @@ public class MainActivity extends LoggingActivity {
         });
 
         // обработка кнопки Next
-        nextButton = findViewById(R.id.next_button);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // move to next question
-                if (currantQuestionIndex == mQuestionBank.length - 1) {
-                    currantQuestionIndex = 0;
+                if (currentQuestionIndex == mQuestionBank.length - 1) {
+                    currentQuestionIndex = 0;
                 } else {
-                    currantQuestionIndex++;
+                    currentQuestionIndex++;
                 }
                 // apply question
                 applyCurrentQuestion();
@@ -71,15 +71,14 @@ public class MainActivity extends LoggingActivity {
         });
 
         // обработка кнопки Previous
-        previousButton = findViewById(R.id.previous_button);
         previousButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // move to previous question
-                if (currantQuestionIndex == 0) {
-                    currantQuestionIndex = mQuestionBank.length - 1;
+                if (currentQuestionIndex == 0) {
+                    currentQuestionIndex = mQuestionBank.length - 1;
                 } else {
-                    currantQuestionIndex--;
+                    currentQuestionIndex--;
                 }
                 // apply question
                 applyCurrentQuestion();
@@ -94,7 +93,7 @@ public class MainActivity extends LoggingActivity {
 
     // получаем текущий вопрос и ответ на него из базы данных
     private Question getCurrentQuestion() {
-        return mQuestionBank[currantQuestionIndex];
+        return mQuestionBank[currentQuestionIndex];
     }
 
     // проверяем правильность ответа пользователем
@@ -105,8 +104,6 @@ public class MainActivity extends LoggingActivity {
 
     // выводим всплывающее сообщение о правильности ответа пользователем
     private void showToast(int textId) {
-        Toast toast = Toast.makeText(MainActivity.this, textId, Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.CENTER, 0, - 250);
-        toast.show();
+        Toast.makeText(MainActivity.this, textId, Toast.LENGTH_SHORT).show();
     }
 }
