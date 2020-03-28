9b121f0673181fd031e65d4b9d515b64bcaf3f3a
Jekson <jeksonshar@gmail.com>
2020-03-28 23:01:10 +0200

    fixed cheat result reset after rotate

--- app/src/main/java/com/javernaut/gquiz/CheatActivity.java
+++ app/src/main/java/com/javernaut/gquiz/CheatActivity.java
@@ -7,24 +7,43 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
 public class CheatActivity extends LoggingActivity {
 
     private static final String KEY_CORRECT_ANSWER = "key_correct_answer";
+    private static final String RESULT_SAVED = "result_saved";
+
+    private int result = 0;
+    private boolean correctAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
 
-        findViewById(R.id.show_correct_answer).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean correctAnswer = getIntent().getBooleanExtra(KEY_CORRECT_ANSWER, false);
+        if (savedInstanceState != null) {
+            result = savedInstanceState.getInt(RESULT_SAVED);
+            if (result > 0){
+                correctAnswer = savedInstanceState.getBoolean(KEY_CORRECT_ANSWER);
 
                 TextView correctAnswerView = findViewById(R.id.correct_answer);
                 correctAnswerView.setText(String.valueOf(correctAnswer));
 
                 setResult(Activity.RESULT_OK);
+            }
+        }
+
+        findViewById(R.id.show_correct_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                correctAnswer = getIntent().getBooleanExtra(KEY_CORRECT_ANSWER, false);
+
+                TextView correctAnswerView = findViewById(R.id.correct_answer);
+                correctAnswerView.setText(String.valueOf(correctAnswer));
+
+                setResult(Activity.RESULT_OK);
+                result++;
             }
         });
     }
@@ -33,5 +52,12 @@ public class CheatActivity extends LoggingActivity {
         Intent intent = new Intent(context, CheatActivity.class);
         intent.putExtra(KEY_CORRECT_ANSWER, correctAnswer);
         return intent;
+    }
+
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putBoolean(KEY_CORRECT_ANSWER, correctAnswer);
+        outState.putInt(RESULT_SAVED, result);
     }
 }
